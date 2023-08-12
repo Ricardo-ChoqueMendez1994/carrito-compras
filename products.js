00ce9d7ce4fb6c87cff0d76700b2751d041d86b0
Richie <choquemendezr@gmail.com>
2023-08-12 18:34:22 -0500

    Agrego el proyecto

---
+++ products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Camiseta",
+        precio: 87000,
+        img : 
+            "https://jumbocolombiaio.vtexassets.com/arquivos/ids/214018-800-800?v=637814276716370000&width=800&height=800&aspect=true"
+    },
+    {
+        id: 2,
+        nombre: "PantaLon",
+        precio: 155000,
+        img: 
+            "https://indusbord.cl/wp-content/uploads/2018/04/pantalon-jeans-001-hombre-1.jpg"
+    },
+    {
+        id: 3,
+        nombre: "Zapatos",
+        precio: 287000,
+        img:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCDP7PPvC6qJyGPqnZcLxMaSf6mkiMdCCEew&usqp=CAU"
+    },
+    {
+        id: 4,
+        nombre: "Calcetines",
+        precio: 17000,
+        img: 
+            "https://http2.mlstatic.com/D_NQ_NP_840447-MCO53433013719_012023-O.webp"
+    },
+    {
+        id: 5,
+        nombre: "Corbata",
+        precio: 87000,
+        img:
+            "https://static.vecteezy.com/system/resources/previews/014/181/398/non_2x/black-tie-icon-flat-style-vector.jpg"
+    }
+]
